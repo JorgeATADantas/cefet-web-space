e1bbbcb95d801a70c92f85578648ee3adc5426db
Jorge Dantas <jatad.tech@gmail.com>
2026-04-01 06:58:35 -0300

    Exercicio3-concluido

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+//Selecionar todos os botões Expandir-Retrair
+let botaoExpandirRetrair = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let botao of botaoExpandirRetrair) {
+  botao.addEventListener("click", function(e) {
+    let evento=e.currentTarget; // Descobre qual é a target do evento
+
+    evento.parentNode.classList.toggle("expandido"); //Aplica a alteração ao parágrafo pai
+    
+    //Alternando os botões + para - e - para +
+    botao.innerText === "+" ? (botao.innerText = "-") : (botao.innerText = "+");
+  });
+}
